2eb430f629f2ef8b701ef32e2602c63a9d612061
Don_Phillips <don.phillips@broadcom.com>
2024-11-26 19:51:30 -0700

    Moved file read into its own function. Added report utilizing the word and character count functions.

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 def main():
+    file_path_and_name = "books/frankenstein.txt"
+    file_contents = read_file(file_path_and_name)
+    wc = word_count(file_contents)
+    char_count_dict = character_count(file_contents)
+    print(f"--- Begin report of {file_path_and_name} ---")
+    print(f"{wc} words found in the document\n")
+    for char in char_count_dict:
+        print(f"The '{char}' character was found {char_count_dict[char]} times")
+    print("--- End report ---")
+
+
+def read_file(file_name):
+    base_path = "/home/donp/boot.dev/workspace/github.com/dpnorth/bookbot/"
+    full_path = base_path + file_name
     try:
-        with open("/home/donp/boot.dev/workspace/github.com/dpnorth/bookbot/books/frankenstein.txt") as f:
+        with open(full_path) as f:
             file_contents = f.read()
-            #print(file_contents)
-            #wc = word_count(file_contents)
-            #print(f"word count {wc}")
-            print(character_count(file_contents))
     except Exception as e:
         print(f"Encountered exception {e}")
+    return file_contents
+
 
 def word_count(text):
     words = text.split()
     return len(words)
+
 
 def character_count(text):
     low_text = text.lower()
@@ -23,5 +36,6 @@ def character_count(text):
         else:
             char_cnt_dict[char] = 1
     return char_cnt_dict
+
 
 main()
