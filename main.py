b49c77214189358639fe38c7c8bb2c03b3f5c4fc
Don_Phillips <don.phillips@broadcom.com>
2024-12-05 12:54:23 -0700

    Added sorting to the report Created sort_dict(), sort_on() and sort_dict_list() functions for two separate ways of sorting Switched the report to the sorted dictionary Restricted the report to alpha characters only Moved character_count()

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
+#!/usr/bin/python3
 def main():
     file_path_and_name = "books/frankenstein.txt"
     file_contents = read_file(file_path_and_name)
     wc = word_count(file_contents)
     char_count_dict = character_count(file_contents)
+    sorted_cc_dict = sort_dict(char_count_dict)
+    #sorted_cc_dict = sort_dict_list(char_count_dict)
+
     print(f"--- Begin report of {file_path_and_name} ---")
     print(f"{wc} words found in the document\n")
-    for char in char_count_dict:
-        print(f"The '{char}' character was found {char_count_dict[char]} times")
+    #for char in char_count_dict:
+        #print(f"The '{char}' character was found {char_count_dict[char]} times")
+    for char in sorted_cc_dict:
+        if (char.isalpha()):
+            print(f"The '{char}' character was found {sorted_cc_dict[char]} times")
     print("--- End report ---")
+
+
+def character_count(text):
+    low_text = text.lower()
+    char_cnt_dict = {}
+    for i in range(0, len(low_text)):
+        char = low_text[i]
+        if (char in char_cnt_dict):
+            char_cnt_dict[char] += 1
+        else:
+            char_cnt_dict[char] = 1
+    return char_cnt_dict
 
 
 def read_file(file_name):
@@ -21,21 +40,33 @@ def read_file(file_name):
     return file_contents
 
 
+def sort_dict(dict_in):
+    sorted_dict = {}
+    for key in sorted(dict_in, key=dict_in.get, reverse=True):
+        sorted_dict[key] = dict_in[key]
+    return sorted_dict
+
+
+def sort_dict_list(dict_in):
+    sorted_dict = {}
+    dict_list = []
+    for key in dict_in:
+        dict_list.append({"key": key, "value" : dict_in[key]})
+    dict_list.sort(reverse=True, key=sort_on)
+    for i in range(0, len(dict_list)):
+        key = dict_list[i]["key"]
+        value = dict_list[i]["value"]
+        sorted_dict[key] = value
+    return sorted_dict
+
+
+def sort_on(dict):
+    return dict["value"]
+
+
 def word_count(text):
     words = text.split()
     return len(words)
-
-
-def character_count(text):
-    low_text = text.lower()
-    char_cnt_dict = {}
-    for i in range(0, len(low_text)):
-        char = low_text[i]
-        if (char in char_cnt_dict):
-            char_cnt_dict[char] += 1
-        else:
-            char_cnt_dict[char] = 1
-    return char_cnt_dict
 
 
 main()
